6f562f4208b628b9858e2c386d86bb1733dad6c5
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-06-20 13:01:27 +0600

    added required Auth

---
+++ src/middlewares/requireAuth.js
@@ -0,0 +1,19 @@
+import { fromNodeHeaders } from "better-auth/node"
+import { auth } from "../lib/auth.js"
+
+export const requireAuth = async (req, res, next) => {
+    try {
+        const session = await auth.api.getSession({
+            headers: fromNodeHeaders(req.headers),
+        })
+
+        if (!session) {
+            return res.status(401).json({ success: false, message: "Unauthorized" })
+        }
+
+        req.user = session.user
+        next()
+    } catch (error) {
+        return res.status(401).json({ success: false, message: "Unauthorized" })
+    }
+}
