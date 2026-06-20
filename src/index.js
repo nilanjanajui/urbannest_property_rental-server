95b89900c84f97c2ddd4a6dfcda751fbcf2b76e4
Nilanjana Jui <nilanjana.csecu@gmail.com>
2026-06-20 21:03:53 +0600

    feat: mount property routes in main server file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import connectDB from "./utils/db.js";
 import healthRoute from "./routes/health.route.js";
+import propertyRouter from './routes/property.route.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/health", healthRoute);
+app.use('/api/properties', propertyRouter);
 
 app.use((req, res) => {
     res.status(404).json({ message: "Route not found" });
